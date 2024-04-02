1b3d9e4f24fa7ecdeb186c18651380d40d1aea2b
Simon Cropp <simon.cropp@gmail.com>
2024-04-02 22:36:33 +1100

    refs and cleanup

--- src/ExtendedFluentValidation/GlobalUsings.cs
+++ src/ExtendedFluentValidation/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿// Global using directives
-
-global using System.Diagnostics.CodeAnalysis;
+﻿global using System.Diagnostics.CodeAnalysis;
 global using DynamicMembers = System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembersAttribute;
 global using DynamicTypes = System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes;
