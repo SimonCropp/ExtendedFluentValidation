fcdeb62354d41a14841d0f18e32344d6f2230888
Simon Cropp <simon.cropp@gmail.com>
2025-05-29 11:10:59 +1000

    Update Tests.cs

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -367,6 +367,88 @@ public class Tests
     }
 
     [Test]
+    public Task Array_NonEmpty()
+    {
+        var validator = new ExtendedValidator<TargetWithArrays>();
+
+        var target = new TargetWithArrays
+        {
+            NotNullable = ["a"],
+            Nullable = ["a"]
+        };
+        var result = validator.Validate(target);
+        return Verify(result);
+    }
+
+    [Test]
+    public Task Array_NonEmpty_ValidateEmptyArrays()
+    {
+        var validator = new ExtendedValidator<TargetWithArrays>(validateEmptyLists: true);
+
+        var target = new TargetWithArrays
+        {
+            NotNullable = ["a"],
+            Nullable = ["a"]
+        };
+        var result = validator.Validate(target);
+        return Verify(result);
+    }
+
+    [Test]
+    public Task Array_Defaults()
+    {
+        var validator = new ExtendedValidator<TargetWithArrays>();
+
+        var target = new TargetWithArrays();
+        var result = validator.Validate(target);
+        return Verify(result);
+    }
+
+    [Test]
+    public Task Array_Defaults_ValidateEmptyArrays()
+    {
+        var validator = new ExtendedValidator<TargetWithArrays>(validateEmptyLists: true);
+
+        var target = new TargetWithArrays();
+        var result = validator.Validate(target);
+        return Verify(result);
+    }
+
+    [Test]
+    public Task Array_Empty_ValidateEmptyArrays()
+    {
+        var validator = new ExtendedValidator<TargetWithArrays>(validateEmptyLists: true);
+
+        var target = new TargetWithArrays
+        {
+            NotNullable = [],
+            Nullable = []
+        };
+        var result = validator.Validate(target);
+        return Verify(result);
+    }
+
+    [Test]
+    public Task Array_Empty()
+    {
+        var validator = new ExtendedValidator<TargetWithArrays>();
+
+        var target = new TargetWithArrays
+        {
+            NotNullable = [],
+            Nullable = []
+        };
+        var result = validator.Validate(target);
+        return Verify(result);
+    }
+
+    class TargetWithArrays
+    {
+        public List<string>? Nullable { get; set; }
+        public List<string> NotNullable { get; set; }
+    }
+
+    [Test]
     public Task Strings_NonEmpty()
     {
         var validator = new ExtendedValidator<TargetWithStrings>();
